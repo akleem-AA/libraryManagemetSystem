fa66a324490ed117299167cb9d4a333d878b896d
mak <mukeem789@gmail.com>
2021-03-07 23:48:27 +0530

    inital commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    Krishan group of college Library Management System |<a  href="#" target="_blank"  > Designed by : Akleem ahmad.</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>  
---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; Krishna group of college- Library Management System |<a href="#" target="_blank"  > Designed by : AKLEEM AHMAD</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
